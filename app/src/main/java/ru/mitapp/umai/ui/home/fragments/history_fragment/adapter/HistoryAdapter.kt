d9fe3a08bdd96433f715a471b4682f73142bc47d
Aidar <achakiev2003@gmail.com>
2021-02-15 17:40:26 +0600

    history fragment

--- app/src/main/java/ru/mitapp/umai/ui/home/fragments/history_fragment/adapter/HistoryAdapter.kt
+++ app/src/main/java/ru/mitapp/umai/ui/home/fragments/history_fragment/adapter/HistoryAdapter.kt
@@ -1,4 +1,41 @@
 package ru.mitapp.umai.ui.home.fragments.history_fragment.adapter
 
-class HistoryAdapter {
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import ru.mitapp.umai.R
+import ru.mitapp.umai.databinding.ItemHistoryBinding
+import ru.mitapp.umai.models.history_model.HistoryModel
+import java.util.ArrayList
+
+class HistoryAdapter(var list: ArrayList<HistoryModel>, var listener: Listener): RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        val binding: ItemHistoryBinding? = DataBindingUtil.bind(android.view.LayoutInflater.from(parent.context).inflate(
+            R.layout.item_history, parent, false))
+
+        return HistoryViewHolder(binding!!)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        holder.onBind(list[position], listener)
+    }
+
+    class HistoryViewHolder(var binding: ItemHistoryBinding): RecyclerView.ViewHolder(binding.root){
+        fun onBind(model: HistoryModel, listener: Listener){
+            binding.model = model
+            itemView.setOnClickListener {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    interface Listener{
+        fun onItemClick(position: Int)
+    }
 }
