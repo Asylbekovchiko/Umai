fe480979e4b1d4bd3214ec6d6c4c6cc91d489c0c
Kudaibergen Abdyldaev <abdyldaevk02@gmail.com>
2021-03-18 17:23:32 +0600

    Закончил DetailHistoryActivity

--- app/src/main/java/ru/mitapp/umai/ui/home/main/transactions/activities/TransactionSuccessActivity.kt
+++ app/src/main/java/ru/mitapp/umai/ui/home/main/transactions/activities/TransactionSuccessActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import ru.mitapp.umai.R
 import ru.mitapp.umai.base.BaseActivity
 import ru.mitapp.umai.databinding.ActivityTransactionSuccessBinding
@@ -55,7 +56,7 @@ class TransactionSuccessActivity
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.share_menu){
-
+            Toast.makeText(this, "Share clicked", Toast.LENGTH_SHORT).show()
         }
         return super.onOptionsItemSelected(item)
     }
